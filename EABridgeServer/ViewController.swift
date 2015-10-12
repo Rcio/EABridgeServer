40fd28c88036d1a09d4daf739551532595fea295
gfwangfei <gfwangfei@oa.gf.com.cn>
2015-10-12 18:21:33 +0800

    Initial Commit

---
+++ EABridgeServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EABridgeServer
+//
+//  Created by rcio on 15/10/12.
+//  Copyright (c) 2015年 rcio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
